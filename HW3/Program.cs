46672ae8204f9bddd5968457cbff9f968dd92126
Aleksandr <aldrobakha@gmail.com>
2022-04-15 22:32:45 +0300

    add HW3

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -1,22 +1,34 @@
-﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
 // 6 -> да
 // 7 -> да
 // 1 -> нет
 
+int Number = getNumber();
+string unswer = Analyze(Number);
+Console.WriteLine(unswer);
+
+int getNumber()
+{
 Console.Write("Введите порядковый номер дня недели: ");
 int Number = Convert.ToInt32(Console.ReadLine());
-
-
-if(Number < 6)
-{
-    Console.Write("Нет");
-}
-else if (Number < 8)
-{
-    Console.Write("Да");
-}
-else
-{
-    Console.Write("Неверный порядковый номер дня недели");
+return Number;
 }
 
+string Analyze(int Number)
+{
+    string unswer;
+    if(Number < 6)
+    {
+        unswer = "Нет";
+    }
+    else if (Number < 8)
+    {
+        unswer = "Да";
+    }
+    else
+    {
+        unswer = "Неверный порядковый номер дня недели";
+    }
+    return unswer;
+}
